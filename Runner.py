bb4e4a7dd805acb4ed51d87f3fb4ed8a1b4f9835
Mahmoud Hussien Mohamed <69102090+MahmoudHussienMohamed@users.noreply.github.com>
2022-08-21 16:50:38 +0200

    Update Runner.py

--- Runner.py
+++ Runner.py
@@ -4,7 +4,7 @@ import sys
 
 def main():
     if len(sys.argv) != 5:
-        sys.exit("Usage: python GA.py [ITERATION_NUMBER] [POPULATION_NUMBER] [BITS_NUMBER] [SENSOR_RADIUS]")
+        sys.exit("Usage: python Runner.py [ITERATION_NUMBER] [POPULATION_NUMBER] [BITS_NUMBER] [SENSOR_RADIUS]")
     n_iter, n_pop, n_bits, radius = map(int, sys.argv[1:])
     cross_r = 0.9
     mut_r = 1.0 / n_bits
@@ -12,5 +12,6 @@ def main():
     GA.start()
     win = window(GA)
     win.show()
+    
 if __name__ == '__main__':
     main()
